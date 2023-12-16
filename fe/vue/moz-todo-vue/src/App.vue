6cd5f28414decf46fbf8e8b63bc39fabb22d0e63
supra.wang <supra.cn@gmail.com>
2023-12-17 02:35:28 +0800

    vue

---
+++ fe/vue/moz-todo-vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <h1>To-Do List</h1>
+    <ul>
+      <li>
+        <to-do-item label="supra test" :done="true" ></to-do-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from "./components/ToDoItem.vue";
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ToDoItem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
